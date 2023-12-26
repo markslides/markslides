fb86760a9563d650c26d341edd784b8278e8fc48
Inje Lee <inje@soaple.io>
2023-12-26 15:46:57 +0900

    Remove themes except default theme temporarily.

---
+++ packages/themes/src/index.ts
@@ -0,0 +1,25 @@
+// import red from '@/themes/red';
+// import green from '@/themes/green';
+// import blue from '@/themes/blue';
+
+type MarkSlidesTheme = {
+    name: string;
+    css: string;
+};
+
+const themes: MarkSlidesTheme[] = [
+    // {
+    //     name: 'red',
+    //     css: red,
+    // },
+    // {
+    //     name: 'green',
+    //     css: green,
+    // },
+    // {
+    //     name: 'blue',
+    //     css: blue,
+    // },
+];
+
+export default themes;
