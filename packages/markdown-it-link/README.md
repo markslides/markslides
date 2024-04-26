1536855e9faa5f17d3eab0f5c353a89176e03730
Inje Lee <inje@soaple.io>
2024-04-26 11:05:27 +0900

    markdown-it-link plugin is implemented.

---
+++ packages/markdown-it-link/README.md
@@ -0,0 +1,19 @@
+# markdown-it-link
+Link plugin for markdown-it
+
+## Installation
+
+```bash
+npm install --save @markslides/markdown-it-link
+```
+
+## Usage
+
+```tsx
+import markdownItMermaid from '@markslides/markdown-it-link';
+
+
+```
+
+## License
+This package releases under the MIT License.
---
+++ packages/markdown-it-link/src/index.ts
@@ -0,0 +1,31 @@
+import MarkdownIt from 'markdown-it';
+import Token from 'markdown-it/lib/token';
+import Renderer from 'markdown-it/lib/renderer';
+
+const urlRegExp =
+    /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+const markdownItLink = (md: MarkdownIt) => {
+    var defaultRender =
+        md.renderer.rules.link_open ||
+        function (tokens, idx, options, env, self) {
+            return self.renderToken(tokens, idx, options);
+        };
+
+    md.renderer.rules.link_open = function (
+        tokens: Token[],
+        idx: number,
+        options: MarkdownIt.Options,
+        env: any,
+        self: Renderer
+    ) {
+        const href = tokens[idx].attrGet('href');
+        if (href && urlRegExp.test(href)) {
+            tokens[idx].attrSet('target', '_blank');
+        }
+
+        return defaultRender(tokens, idx, options, env, self);
+    };
+};
+
+export default markdownItLink;
