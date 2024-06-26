42c0ee4b60e38843483fb0ebdf2ab1d651f384eb
Inje Lee <inje@soaple.io>
2024-06-26 16:28:59 +0900

    Add logo to header.

--- apps/web/components/base/editor/EditorLayout/EditorHeader.tsx
+++ apps/web/components/base/editor/EditorLayout/EditorHeader.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@markslides/ui/flex';
 import { Button } from '@markslides/ui/button';
-import { Text } from '@markslides/ui/text';
+import { Image } from '@markslides/ui/image';
 import { PlayIcon } from 'lucide-react';
 import useAppDispatch from '@/redux/hooks/useAppDispatch';
 import { setIsSlideShowMode } from '@/redux/slices/appSlice';
@@ -11,9 +11,18 @@ function EditorHeader() {
     return (
         <Flex
             height='40px'
+            paddingLeft='16px'
+            paddingRight='16px'
+            alignItems='center'
             justifyContent='space-between'
             backgroundColor='white'>
-            <Text color='black'>MarkSlides</Text>
+            <Image
+                src='logo_with_text.svg'
+                style={{
+                    width: 'auto',
+                    height: '32px',
+                }}
+            />
 
             <Button
                 icon={<PlayIcon size={14} />}
