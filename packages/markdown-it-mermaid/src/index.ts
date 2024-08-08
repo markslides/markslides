422a670c73d2e0795ec459070472db2e9bb53bf8
Inje Lee <inje@soaple.io>
2024-08-08 14:12:28 +0900

    Support options for markdownItMermaid.

--- packages/markdown-it-mermaid/src/index.ts
+++ packages/markdown-it-mermaid/src/index.ts
@@ -1,7 +1,7 @@
 import MarkdownIt from 'markdown-it';
 import Token from 'markdown-it/lib/token';
 import Renderer from 'markdown-it/lib/renderer';
-import mermaid from 'mermaid';
+import mermaid, { type MermaidConfig } from 'mermaid';
 
 const mermaidChart = (code: string) => {
     try {
@@ -15,38 +15,41 @@ const mermaidChart = (code: string) => {
 // TODO: Inject this value from outside
 const isDarkMode = false;
 
-const markdownItMermaid = (md: MarkdownIt) => {
+const markdownItMermaid = (config?: MermaidConfig) => {
     mermaid.initialize({
         theme: isDarkMode ? 'default' : 'dark',
         darkMode: isDarkMode,
         fontFamily: 'ui-monospace',
         altFontFamily: 'monospace',
         startOnLoad: true,
+        ...config,
     });
 
-    // @ts-ignore
-    md.mermaid = mermaid;
+    return (md: MarkdownIt) => {
+        // @ts-ignore
+        md.mermaid = mermaid;
 
-    const original =
-        md.renderer.rules.fence ||
-        function (tokens, idx, options, env, self) {
-            return self.renderToken(tokens, idx, options);
+        const original =
+            md.renderer.rules.fence ||
+            function (tokens, idx, options, env, self) {
+                return self.renderToken(tokens, idx, options);
+            };
+
+        md.renderer.rules.fence = (
+            tokens: Token[],
+            idx: number,
+            options: MarkdownIt.Options,
+            env: any,
+            self: Renderer
+        ) => {
+            const token = tokens[idx];
+            const code = token.content.trim();
+            if (token.info === 'mermaid') {
+                return mermaidChart(code);
+            }
+
+            return original(tokens, idx, options, env, self);
         };
-
-    md.renderer.rules.fence = (
-        tokens: Token[],
-        idx: number,
-        options: MarkdownIt.Options,
-        env: any,
-        self: Renderer
-    ) => {
-        const token = tokens[idx];
-        const code = token.content.trim();
-        if (token.info === 'mermaid') {
-            return mermaidChart(code);
-        }
-
-        return original(tokens, idx, options, env, self);
     };
 };
 
