b5a87ffaf4f9efac6860e5b1aef4b786afc93174
Inje Lee <inje@soaple.io>
2025-09-11 11:44:18 +0900

    Remove old unused themes.

--- packages/themes/src/index.ts
+++ packages/themes/src/index.ts
@@ -1,8 +1,5 @@
 import defaultTheme from '@/generated/default';
 import minimalTheme from '@/generated/minimal';
-// import redTheme from '@/generated/red';
-// import greenTheme from '@/generated/green';
-// import blueTheme from '@/generated/blue';
 
 type MarkSlidesTheme = {
     name: string;
@@ -18,18 +15,6 @@ const themes: MarkSlidesTheme[] = [
         name: 'minimal',
         css: minimalTheme,
     },
-    // {
-    //     name: 'red',
-    //     css: redTheme,
-    // },
-    // {
-    //     name: 'green',
-    //     css: greenTheme,
-    // },
-    // {
-    //     name: 'blue',
-    //     css: blueTheme,
-    // },
 ];
 
 export default themes;
